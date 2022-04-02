33a846335f74fff198f03ca4416ab0fd0627d0aa
Dannycom <58293876+Dannycom@users.noreply.github.com>
2022-04-02 19:10:38 +1300

    Blur element
    added blur function, need to still add the transition from blur to visible on click

---
+++ index.js
@@ -0,0 +1,16 @@
+const textBank = ["andy's text", "joji's text", "angel's text"];
+/**
+ * @function loadDaily displays today's text
+ */
+function loadDaily() {
+    const today = new Date();
+    // One day (24 hours) is 86,400,000 milliseconds
+    const daysPassed = Math.floor(today.getTime() / 86400000);
+    // if a user sets their local machine date to before January 1, 1970 -> negative days since starting date -> negative array index (remainder) -> undefined
+    // modulus formula [ ((a % n ) + n ) % n ] to account for negative values
+    const length = textBank.length;
+    document.getElementById("gameText").innerHTML = textBank[((daysPassed % length) + length) % length];
+}
+window.onload = function () {
+    loadDaily()
+}
