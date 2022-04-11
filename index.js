4b8ab2b6509d852bf634178c1757fabb0dd776d3
senorita-dev <ren.saito.dev@gmail.com>
2022-04-11 15:32:07 +1200

    SP-23 Added Input Box and sanitizing user input
    https://compsci399.atlassian.net/browse/SP-23

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function loadDaily() {
     // if a user sets their local machine date to before January 1, 1970 -> negative days since starting date -> negative array index (remainder) -> undefined
     // modulus formula [ ((a % n ) + n ) % n ] to account for negative values
     const length = textBank.length;
-    const text =  textBank[((daysPassed % length) + length) % length];
+    const text = textBank[((daysPassed % length) + length) % length];
     document.getElementById("gameText").innerHTML = text;
     document.getElementById("gameHidden").innerHTML = text;
 }
@@ -32,9 +32,32 @@ function Getversion() {
     document.getElementById("vers").innerText = "v. Beta";
 }
 
-
+/**
+ * @function sanitizeInput sanitizes the user input as described in SP-23
+ * @param e - the event @type - "beforeinput"
+ * @returns if the text has been changed
+ */
+function sanitizeInput(e, element) {
+    // case: delete or single alphabetical character //TODO: add quotation marks etc? would prefer if the retyping was just words, TBC w team
+    if (e.inputType == 'deleteContentBackward' || e.inputType == 'deleteContentForward' || /^[a-zA-Z()]$/.test(e.data)) {
+        return false;
+    }
+    e.preventDefault();
+    return true;
+}
+/**
+ * @function textInputHandler() uses the input text to search & highlight relevant words
+ * @param e - the event @type - "beforeinput"
+ * @param element - the textarea element inputTextBox
+ */
+function textInputHandler(e, element) {
+    if (!sanitizeInput(e)) { // if the text has been changed
+        //highlight text
+    }
+}
 window.onload = function () {
     loadDaily()
     hideGame()
     Getversion()
+    document.getElementById("inputTextBox").addEventListener("beforeinput", function (e) { textInputHandler(e, this); });
 }
