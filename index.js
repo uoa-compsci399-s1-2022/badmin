07b77042aeb7b7f66490a2604b48361dcdacc1f4
Dannycom <58293876+Dannycom@users.noreply.github.com>
2022-04-23 21:31:50 +1200

    score and combo done
    finished making the score and combo functions

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function showGame() {
     document.getElementById("text-sel").style.display = "none";
     document.getElementById("pageInfo").style.display = "none";
     document.getElementById("title").style.color = "#60525F" 
+    document.getElementById("score").style.display = "grid";
 }
 /**
  * Blur text function
@@ -16,6 +17,7 @@ function hideGame() {
     document.getElementById("gameText").style.display = "none";
     document.getElementById("gameHidden").style.display = "block";
     document.getElementById("gameControls").style.display = "none";
+    document.getElementById("score").style.display = "none";
 }
 
 function pause() {
@@ -58,7 +60,8 @@ let currentSearchIndex = 0;
 function highlightText(element) {
     const gameTextArr = suppliedText.split(" ");
     const gameTextElements = document.getElementById("gameText").children;
-    const searchText = element.value;
+    //const searchText = element.value;
+    const searchText = element.innerText;
     clearPreviousHighlight();
     if (searchText.length >= 1) {
         for (let i = 0; i < gameTextArr.length; i++) {
@@ -94,17 +97,21 @@ function inputHandler(element, event) {
     if (event.code == "Enter" || event.code == "Space") {
         checkUserInput(element);
     }
-    else if (event.code == "ArrowUp" || event.code == "ArrowDown") {
+    else if (event.code == "ArrowUp" || event.code == "ArrowDown" || event.code == "ArrowLeft" || event.code == "ArrowRight") {
         navigateSearchResults(event.code);
+        event.preventDefault();
     }
 }
 
+let score = 0;
+let comboCounter = 0;
+let previousCorrectedTime = null;
 function checkUserInput(element) {
-    if (element.value.length >= 2) {
+    if (element.innerText.length >= 2) {
         let index = -1;
         for (let i = 0; i < Object.keys(correctIndicies).length; i++) {
             if (correctedWordsIndicies.includes(i) == false) {
-                if (correctIndicies[Object.keys(correctIndicies)[i]].replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ") == element.value) {
+                if (correctIndicies[Object.keys(correctIndicies)[i]].replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ") == element.innerText) {
                     index = Object.keys(correctIndicies)[i];
                 }
             }
@@ -113,7 +120,29 @@ function checkUserInput(element) {
             if (correctedWordsIndicies.includes(index) == false) {
                 replaceWord(correctIndicies[index], index);
                 correctedWordsIndicies.push(index);
+                const currentTime = Date.now();
+                if (previousCorrectedTime == null) {
+                    comboCounter = 1;
+                }
+                else if (currentTime - previousCorrectedTime <= 5000) { //5000 milliseconds = 5 seconds
+                    if (comboCounter < 3) {
+                        comboCounter++;
+                    }
+                }
+                else {
+                    comboCounter = 1;
+                }
+                previousCorrectedTime = currentTime;
+                score += 100 * comboCounter;
+                document.getElementById("score").innerText = "score: \n" + score;
+                document.getElementById("combo").innerText = "combo: \n" + comboCounter;
             }
+        }
+        else {
+            comboCounter = 0;
+            score -= 30;
+            document.getElementById("score").innerText = "score: \n" + score;
+            document.getElementById("combo").innerText = "combo: \n" + comboCounter;
         }
     }
 }
@@ -124,10 +153,10 @@ function navigateSearchResults(key) {
         const searchElement = gameTextElements[previousSearchIndicies[currentSearchIndex]].firstElementChild;
         searchElement.outerHTML = `<mark>${searchElement.innerHTML}</mark>`; //clearing the attributes (including style) of the current selected search result
         const length = previousSearchIndicies.length;
-        if (key == "ArrowUp") {
+        if (key == "ArrowUp" || key == "ArrowLeft") {
             currentSearchIndex = (((currentSearchIndex - 1) % length) + length) % length // modulus formula [ ((a % n ) + n ) % n ] to account for negative values
         }
-        if (key == "ArrowDown") {
+        if (key == "ArrowDown" || key == "ArrowRight") {
             currentSearchIndex = (currentSearchIndex + 1) % length // currentSearchIndex can't be negative, so currentSearchIndex + 1 can't be negative => use positive only modulus
         }
         gameTextElements[previousSearchIndicies[currentSearchIndex]].firstElementChild.style.backgroundColor = "lightblue";
@@ -148,7 +177,8 @@ function generateCorrectIndicies() {
 function replaceWord(correctWord, correctedIndex) {
     document.getElementById("gameText").children[correctedIndex].innerText = correctWord;
     document.getElementById("gameText").children[correctedIndex].style.color = "#EDD9A3";
-    document.getElementById("inputTextBox").value = "";
+    //document.getElementById("inputTextBox").value = "";
+    document.getElementById("inputTextBox").innerText= "";
 }
 
 window.onload = function () {
@@ -177,7 +207,9 @@ let timerVar = 0;
 function startTimer() {
     correctIndicies = {};
     correctedWordsIndicies = new Array();
-    document.getElementById("inputTextBox").removeAttribute("disabled");
+    //document.getElementById("inputTextBox").removeAttribute("disabled");
+    //document.getElementById("inputTextBox").focus();
+    document.getElementById("inputTextBox").setAttribute("contenteditable", true);
     document.getElementById("inputTextBox").focus();
     document.getElementById("timer").innerHTML = "";
     loadText();
@@ -185,12 +217,19 @@ function startTimer() {
     timerVar = setInterval(countTimer, 1000);
     showGame();
     generateCorrectIndicies();
+    score = 0;
+    comboCounter = 0;
+    document.getElementById("score").innerText = `score: \n ${score}`;
+    document.getElementById("combo").innerText = `combo: \n ${comboCounter}`;
+    previousCorrectedTime = null;
 }
 
 function stopTimer() {
     clearPreviousHighlight();
-    document.getElementById("inputTextBox").value = "";
-    document.getElementById("inputTextBox").setAttribute("disabled", '');
+    //document.getElementById("inputTextBox").value = "";
+    document.getElementById("inputTextBox").innerText = "";
+    //document.getElementById("inputTextBox").setAttribute("disabled", '');
+    document.getElementById("inputTextBox").setAttribute("contenteditable", false);
     clearInterval(timerVar);
     pause();
 }
